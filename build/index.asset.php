a9d1639ad40e401eac6932626551ae86cab1caf9
sarahcssiqueira <sarahcso@hotmail.com>
2023-04-28 14:26:29 -0300

    refactoring: index.php rewritten with class

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f3ded18f6814ccce37d8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '741c2941f9f2af0608dc');
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Retrieve Github Repos Block
+ * Plugin Name:       Retrieve Github Repositories Block
  * Author:            Sarah Siqueira
  * Author URI:        sarahjobs.com
  * Description:       Gutenberg block to retrieve your public github repositories and show them as a portfolio. Used for millions of developers around the globe, Github does not require a presentation. Easily display your public Github repositories, just set up your github username below.
@@ -13,61 +13,84 @@
  * 
  */
 
-// Register //
-function retrieve_github_block() {
-    register_block_type(
-        __DIR__ ,
-        [
-            'attributes'      => [
-              'githubuser' => [
-                'type'    => 'string',
-              ],
-            ],
+/** Prevent public user to directly access your .php files through URL **/
+if( ! defined( 'ABSPATH' ) ) exit;
 
-        'render_callback' => 'render_retrieved_github_repos',
-        ]
-    );
-}
+class RetrieveGithubBlock {
 
-add_action( 'init', 'retrieve_github_block' );
-
-
-//Render dynamic block //
-function render_retrieved_github_repos( $attributes, $content ) {
-
-    $headers = [
-        "User-Agent: Github Gutenberg Block",
-    ];
-
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_RETURNTRANSFER => true
-    ]);
-    
-    $username = $attributes['githubuser'];
-
-    curl_setopt($curl, CURLOPT_URL, ("https://api.github.com/users/" . $username . "/repos"));
-
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-   
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-        $response;
+    function __construct() {
+      add_action('init', array($this, 'startBlock'));
     }
+  
+    function startBlock() {
 
-    $repositories = json_decode( $response, true );
+        wp_register_script( 'retrieve-github-script' ,
+            plugin_dir_url(__FILE__). '.build/index.js', 
+            array('wp-blocks', 'wp-i18n', 'wp-editor')
+        );
 
-    ob_start();
+        wp_register_style( 'retrieve-github-style',
+            plugin_dir_url(__FILE__) . '.src/style/style.css',
+            array(),
+        );
+      
+        register_block_type(__DIR__,
+            [
+                'attributes'      => [
+                    'githubuser' => [
+                        'type'    => 'string',
+                        ],
+                ],
+                'render_callback' => array($this, 'render_retrieved_github_repos'),
+                //'render_callback' => 'render_retrieved_github_repos',
+                'editor_script' => 'retrieve-github-script',
+                'editor_style' => 'retrieve-github-style'
+            ]
+        );
+      
+    }
     
-?>
-    <h3> <?php esc_html_e('Code Samples') ?> </h3>
+    /**
+	 * Retrieve and display data from API
+	 *
+	 */
+    function render_retrieved_github_repos( $attributes, $content ) {
+        
+        $headers = [
+            "User-Agent: Github Gutenberg Block",
+            ];
 
+        $username = $attributes['githubuser'];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_RETURNTRANSFER => true
+            ]
+        );
+        
+        curl_setopt($curl, CURLOPT_URL, ("https://api.github.com/users/" . $username . "/repos"));
+
+        $response = curl_exec($curl);
+
+        $err = curl_error($curl);
+
+        curl_close($curl);
+    
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $response;
+        }
+
+        $repositories = json_decode( $response, true );
+
+        ob_start(); ?>
+
+        
         <?php foreach ( $repositories as $repo ): ?>
-            
+        
             <article class="">
                 
                 <a 
@@ -83,56 +106,34 @@ function render_retrieved_github_repos( $attributes, $content ) {
                     Go to repository
                     </a>
                 </p>
-
-                <ul>
-                    <li class="">
+                <p class="">
                         <?php $date = strtotime($repo["created_at"])?>
                         <?php echo date('Y/M/d', $date);?>
-                    </li>
-                </ul>
-                <ul>
-                    <li class="">
-                        <?php foreach ($repo["topics"] as $topic) {
-                        echo $topic;}
-                        ?>       
-                    </li>
+                </p>
+                
+                <p class="">
+                    <?php foreach ($repo["topics"] as $topic) {
+                    echo $topic;}
+                    ?>       
+                </p>
+                <p class="">
+                    <?php echo $repo["stargazers_count"]?> stars
+                </p>
 
-                </ul>
-
-                <section class="">
-                    <p class="">
-                        <?php echo $repo["stargazers_count"]?> stars
-                    </p>
-
-                    <p class="">
-                        <?php echo $repo["forks_count"]?> forks
-                    </p>
-
-                </section>
+                <p class="">
+                    <?php echo $repo["forks_count"]?> forks
+                </p>
 
             </article>
+            
+        <?php endforeach; ?>
 
-            <?php endforeach; ?> 
-    <?php
+    <?php return ob_get_clean();
 
-    return ob_get_clean();
-}
-       
+    }
 
-// Enqueues //
-function retrieve_github_enqueues() {
-
-    wp_enqueue_script( 'retrieve-github' ,
-        plugin_dir_url(__FILE__). '.build/index.js', 
-        array('wp-blocks', 'wp-i18n', 'wp-editor')
-    );
-
-    wp_enqueue_style(
-        'retrieve-github',
-        plugin_dir_url(__FILE__) . '.src/style/style.css',
-        array(),
-    );
 }
 
-add_action( 'enqueue_block_editor_assets', 'retrieve_github_enqueues' );
+$retrieveGithubBlock = new RetrieveGithubBlock();
+
 ?>
