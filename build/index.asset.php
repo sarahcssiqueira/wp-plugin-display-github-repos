436cf754a4ccb02519b7feb63d9d23c8ef4e9bcf
sarahcssiqueira <sarahcso@hotmail.com>
2023-06-08 15:35:35 -0300

    refactor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '741c2941f9f2af0608dc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f7e3cf45914aaab3f1ea');
--- retrieve-github-repositories.php
+++ retrieve-github-repositories.php
@@ -4,38 +4,35 @@
  * Author:            Sarah Siqueira
  * Author URI:        sarahjobs.com
  * Description:       Gutenberg block to retrieve your public github repositories and show them as a portfolio. Used for millions of developers around the globe, Github does not require a presentation. Easily display your public Github repositories, just set up your github username below.
- * Version:           0.2.0
+ * Version:           1.0.0
  * Requires at least: 5.4
  * Requires PHP:      7.0
  * License:           ''
  * License URI:       ''
  * Text Domain:       github portfolio
- * 
  */
 
-/** Exit if accessed directly **/
-if( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * Exit if accessed directly 
+ **/
+if(! defined('ABSPATH') ) { exit;
+}
 
-class RetrieveGithubBlock {
+new RetrieveGithubBlock(); // Initialize
 
-    /** Can be considered a weak solution to handle with hooks cause can misunderstands the role of the constructor, but in this case I believe it's ok. **/
-    function __construct() {
-      add_action('init', array($this, 'startBlock'));
+class RetrieveGithubBlock
+{
+
+    function __construct()
+    {
+        add_action('init', array($this, 'block_register'));
+        add_action('enqueue_block_editor_assets', array($this, 'block_enqueues'));
     }
-  
-    function startBlock() {
-
-        wp_register_script( 'retrieve-github-script' ,
-            plugin_dir_url(__FILE__). '.build/index.js', 
-            array('wp-blocks', 'wp-i18n', 'wp-editor')
-        );
-
-        wp_register_style( 'retrieve-github-style',
-            plugin_dir_url(__FILE__) . '.src/style/style.css',
-            array(),
-        );
-      
-        register_block_type(__DIR__,
+    
+    function block_register()
+    {
+        register_block_type(
+            __DIR__,
             [
                 'attributes'      => [
                     'githubuser' => [
@@ -43,35 +40,47 @@ class RetrieveGithubBlock {
                         ],
                 ],
                 'render_callback' => array($this, 'render_retrieved_github_repos'),
-                //'render_callback' => 'render_retrieved_github_repos',
-                'editor_script' => 'retrieve-github-script',
-                'editor_style' => 'retrieve-github-style'
-            ]
+                ]
         );
-      
     }
-    
-    /**
-	 * Retrieve and display data from API
-	 *
-	 */
-    function render_retrieved_github_repos( $attributes, $content ) {
+
+
+    function block_enqueues()
+    {
+        wp_enqueue_script( 
+            'retrieve-github',
+            plugin_dir_url(__FILE__). '.build/index.js', 
+            array('wp-blocks', 'wp-i18n', 'wp-editor')
+        );
+
+        wp_enqueue_style( 
+            'retrieve-github',
+            plugin_dir_url(__FILE__) . '.src/style/style.css',
+            array(),
+        );
+    }
         
+    /**
+     * Retrieve and display data from API
+     */
+    function render_retrieved_github_repos( $attributes, $content )
+    {
         $headers = [
             "User-Agent: Github Gutenberg Block",
             ];
-        
+            
         /* Get the username input */
         $username = $attributes['githubuser'];
 
         $curl = curl_init();
 
-        curl_setopt_array($curl, [
+        curl_setopt_array(
+            $curl, [
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_RETURNTRANSFER => true
-            ]
+                ]
         );
-        
+            
         curl_setopt($curl, CURLOPT_URL, ("https://api.github.com/users/" . $username . "/repos"));
 
         $response = curl_exec($curl);
@@ -79,7 +88,7 @@ class RetrieveGithubBlock {
         $err = curl_error($curl);
 
         curl_close($curl);
-    
+        
         if ($err) {
             echo "cURL Error #:" . $err;
         } else {
@@ -87,37 +96,36 @@ class RetrieveGithubBlock {
         }
 
         /* Decode the response */
-        $repositories = json_decode( $response, true );
+        $repositories = json_decode($response, true);
 
         ob_start(); ?>
 
-        
-        <?php foreach ( $repositories as $repo ): ?>
-        
-            <div class="">
-
-                <a href="<?php echo $repo["svn_url"]?>"class=""> <?php echo $repo["name"] ?></a>
-
-                <p class=""><?php echo $repo["description"] ?><a href="<?php $repo["svn_url"] ?>"class="">Go to repository</a></p>
-
-                <p class=""><?php $date = strtotime($repo["created_at"])?><?php echo date('Y/M/d', $date);?></p>
-                
-                <p class=""><?php foreach ($repo["topics"] as $topic) {echo $topic;}?></p>
-
-                <p class=""><?php echo $repo["stargazers_count"]?> stars</p>
-
-                <p class=""><?php echo $repo["forks_count"]?> forks </p>
-
-            </div>
             
-        <?php endforeach; ?> 
+            <?php foreach ( $repositories as $repo ): ?>
+            
+                <div class="">
 
-    <?php return ob_get_clean();
+                    <a href="<?php echo $repo["svn_url"]?>"class=""> <?php echo $repo["name"] ?></a>
+
+                    <p class=""><?php echo $repo["description"] ?><a href="<?php $repo["svn_url"] ?>"class="">Go to repository</a></p>
+
+                    <p class=""><?php $date = strtotime($repo["created_at"])?><?php echo date('Y/M/d', $date);?></p>
+                    
+                    <p class=""><?php foreach ($repo["topics"] as $topic) {echo $topic;
+                                }?></p>
+
+                    <p class=""><?php echo $repo["stargazers_count"]?> stars</p>
+
+                    <p class=""><?php echo $repo["forks_count"]?> forks </p>
+
+                </div>
+                
+            <?php endforeach; ?> 
+
+        <?php return ob_get_clean();
 
     }
 
 }
-
-$retrieveGithubBlock = new RetrieveGithubBlock();
 
 ?>
